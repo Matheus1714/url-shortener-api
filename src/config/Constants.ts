62629f8e9be2b4109d14aaf060f35b94cfdec91d
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-08 17:34:49 -0300

    config dotenv

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 export const config = {
-    API_URL: "http://localhost:5000",
-    MONGO_CONNECTION: "mongodb+srv://testbootcamp:oen8mVfsZUDgwZBJ@cluster0.mae3lfi.mongodb.net/?retryWrites=true&w=majority",
+    API_URL: process.env.API_URL,
+    MONGO_CONNECTION: process.env.MONGO_CONNECTION
 }
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import express from 'express'
 import { URLController } from './controller/URLControle'
 import { MongoConnection } from './database/MongoConnection'
 
+require('dotenv').config()
+
 const api = express()
 api.use(express.json())
 
 const database = new MongoConnection()
 database.connect()
-
 
 const urlControler = new URLController()
 api.post('/shorten', urlControler.shorten)

9a17cb75b66dcb9f4e9e660ba2861e75bc59e072
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-08 16:46:42 -0300

    javascript generated files

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: "mongodb+srv://testbootcamp:oen8mVfsZUDgwZBJ@cluster0.mae3lfi.mongodb.net/?retryWrites=true&w=majority",
+};
+//# sourceMappingURL=Constants.js.map

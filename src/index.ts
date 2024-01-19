925efdb11b207f3c0930d055858cc29033fde8a3
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-19 16:37:46 -0300

    remove files unused

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { connect } from 'mongoose';
-import { config } from './config/Constants';
 import express from 'express'
 import { URLController } from './controller/URLControle'
 import { MongoConnection } from './database/MongoConnection'
@@ -13,6 +11,7 @@ const database = new MongoConnection()
 database.connect()
 
 const urlControler = new URLController()
+
 api.post('/shorten', urlControler.shorten)
 api.get('/:hash', urlControler.redirect)
 

81248a49b1b034be6bb6367b37026229fb01fb5e
matheus1714 <matheussilvamartins1714@gmail.com>
2025-01-26 01:21:21 -0300

    config; add node env

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,6 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      
       DB_HOST: string;
       DB_PORT: string;
       DB_NAME: string;
